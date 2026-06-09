9bdeeb5e0dad11d4f9ab149320f0d69c34e3d05e
SntSwetlana <snt.swetlana@gmail.com>
2026-06-09 09:42:15 +0300

    vercel version

--- api/bot.ts
+++ api/bot.ts
@@ -1,27 +1,20 @@
 import "dotenv/config";
-import { Bot } from "grammy";
+import { Bot, webhookCallback } from "grammy";
 
 const token = process.env.BOT_TOKEN;
 
-console.log("token");
 if (!token) {
   throw new Error("BOT_TOKEN is missing");
 }
 
 const bot = new Bot(token);
-console.log("new bot");
 
 bot.command("start", async (ctx) => {
-  await ctx.reply("📚 English Through Articles\n\nUse /articles");
+  await ctx.reply("📚 English Through Articles\n\nWelcome!");
 });
-console.log("start ");
 
 bot.command("articles", async (ctx) => {
   await ctx.reply("🧬 Wildlife Returns to Chernobyl");
 });
 
-console.log("articles ");
-
-console.log("Bot started");
-
-bot.start();
+export default webhookCallback(bot, "http");
