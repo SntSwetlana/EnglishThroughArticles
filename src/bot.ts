77ad2f5cce839a476d58c5d18af401c9e2ce94f1
SntSwetlana <snt.swetlana@gmail.com>
2026-06-09 09:29:56 +0300

    .

--- src/bot.ts
+++ src/bot.ts
@@ -3,19 +3,24 @@ import { Bot } from "grammy";
 
 const token = process.env.BOT_TOKEN;
 
+console.log("token");
 if (!token) {
   throw new Error("BOT_TOKEN is missing");
 }
 
 const bot = new Bot(token);
+console.log("new bot");
 
 bot.command("start", async (ctx) => {
   await ctx.reply("📚 English Through Articles\n\nUse /articles");
 });
+console.log("start ");
 
 bot.command("articles", async (ctx) => {
   await ctx.reply("🧬 Wildlife Returns to Chernobyl");
 });
+
+console.log("articles ");
 
 console.log("Bot started");
 
